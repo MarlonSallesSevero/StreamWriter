452720ce146b01130a48369d6f5fab189bfc2c19
Marlon Salles <marlonsalles@hotmail.com>
2022-05-01 15:33:58 -0300

    StreamWrite e a operação ToUpper()

---
+++ StreamWrite/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+namespace Using_Block
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\marlo\teste001.txt";
+            string targetPath = @"C:\Users\marlo\teste002.txt";
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach(string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+             
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
